15c8a999e211b6671428ab7f0481cce4d88b4653
Anthony Laliberte <anthony.laliberte.1@ulaval.ca>
2024-12-29 13:19:50 -0500

    started drag drop support

---
+++ FinanceManager/Utils/DragDropExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Utils
+{
+    public static class DragDropExtensions
+    {
+        public static void AddDragDropHandler(this Interactive control, EventHandler<DragEventArgs> dropHandler, EventHandler <DragEventArgs> dragOverHanlder = default)
+        {
+            DragDrop.SetAllowDrop(control, true);
+            control.AddHandler(DragDrop.DropEvent, dropHandler);
+            if(dragOverHanlder != null)
+            {
+                control.AddHandler(DragDrop.DragOverEvent, dragOverHanlder);
+            }
+            else
+            {
+                control.AddHandler(DragDrop.DragOverEvent, DefaultDragOverHandler);
+            }
+        }
+
+        private static void DefaultDragOverHandler(object sender, DragEventArgs e)
+        {
+            e.DragEffects = DragDropEffects.Move;
+        }
+    }
+}
--- FinanceManager/Views/FinanceView.axaml.cs
+++ FinanceManager/Views/FinanceView.axaml.cs
@@ -1,8 +1,14 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.PropertyGrid.Controls;
+using FinanceManager.Utils;
+using FinanceManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FinanceManager.Views;
 
@@ -11,5 +17,16 @@ public partial class FinanceView : UserControl
     public FinanceView()
     {
         InitializeComponent();
+        dgFinancialData.AddDragDropHandler(Drop);
+    }
+
+    private void Drop(object sender, DragEventArgs e)
+    {
+        List<string> files = e.Data.GetFiles()?.Select(x => x.Path.LocalPath).ToList();
+        if(files == null)
+        {
+            return;
+        }
+        //do something here
     }
 }
